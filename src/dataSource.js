b551b8e4d823b9503d4bb1d4ed39fb1e7a3472d4
Bharath Gohrani <80106630+bgohrani@users.noreply.github.com>
2026-09-20 13:47:13 +0530

    Update dataSource.js

--- src/dataSource.js
+++ src/dataSource.js
@@ -4,5 +4,8 @@ export const GOOGLE_SHEET_ID =
 export const GOOGLE_SHEET_CSV_URL =
   `https://7msqawcejrpculsy.public.blob.vercel-storage.com/authorization_data_120000_fraud_chargeback%20%281%29.csv`;
 
+export const AUTHENTICATION_DATA_URL =
+  "https://4oupentp6qrm9rag.public.blob.vercel-storage.com/authentication_data.csv";
+
 export const AI_API_URL =
   "https://payments-ai-insights.bgohrani.workers.dev/";

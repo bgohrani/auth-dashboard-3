6fcb6ad907520088d53d39fe3a576bf751343f22
Bharath Gohrani <80106630+bgohrani@users.noreply.github.com>
2026-09-21 11:14:54 +0530

    Update dataSource.js

--- src/dataSource.js
+++ src/dataSource.js
@@ -2,10 +2,10 @@ export const GOOGLE_SHEET_ID =
   "1tadSeuYgEliS2cOkH-9uR31NUDS7Mt2PXjiB_-1EW44";
 
 export const GOOGLE_SHEET_CSV_URL =
-  `https://2ihjqj1vkxppmwbp.public.blob.vercel-storage.com/final-autho-data.csv`;
+  `https://docs.google.com/spreadsheets/d/1fbtu3ikcGTPh6YA98-xOBAT3Ml55ZtngKe_gsxMVEvs/gviz/tq?tqx=out%3Acsv&utm_source=chatgpt.com`;
 
 export const AUTHENTICATION_DATA_URL =
-  "https://4oupentp6qrm9rag.public.blob.vercel-storage.com/authentication_data.csv";
+  "https://docs.google.com/spreadsheets/d/1lavWiL4nV83AMHbg0trGVhFkTuJ9gvS3nwC77svnLTc/gviz/tq?tqx=out%3Acsv&utm_source=chatgpt.com";
 
 export const check_var = "1234";
 
